546f507433f2bb59eec1bb015cead55133d71dba
kkdewangan <dewangan.kamal@gmail.com>
2018-08-17 12:42:41 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,100 @@
+package com.naturallatex.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by kamald on 8/14/18.
+ */
+@Entity
+
+public class Customer implements Serializable{
+
+    @Id
+    @Column(name = "cust_id")
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
+    //@SequenceGenerator(sequenceName = "customer_seq", allocationSize = 1, name = "customer_seq")
+    Long id;
+
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String email;
+
+    private String password;
+    private String phone;
+    private String fax;
+    private Boolean status;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
